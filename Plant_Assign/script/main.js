87ab04ac4c87d6cdf1a3343169eb71d61e2b1f08
KrishnaPrasadArisetty <155144903+KrishnaPrasadArisetty@users.noreply.github.com>
2024-12-14 07:09:04 +0000

    TEST

--- Plant_Assign/script/main.js
+++ Plant_Assign/script/main.js
@@ -366,10 +366,10 @@ require(["DS/DataDragAndDrop/DataDragAndDrop", "DS/PlatformAPI/PlatformAPI", "DS
 					return { id: index + 1, Plant: plantObject.title}}),comWidget.getAvaliablePlantTable(),"AvaliablePlantsTable"));
 				
 				AssignedClasses.classes.forEach((Plantclass,index) => {
-					ClassTableData.push({id:index+1, Plant:Plantclass.title, Seq:"1",Status:"Current",MFG_Change: "MCONAME", MFG_Status: "Create",Change:"CA-00000777", ChangeStatus:"In Work", OracleTemplate: Plantclass.PlantOracleTemplate, ERPStatus:"true",ERP_Export:Plantclass.PlantERPExoprt, Lead_Plant:Plantclass.PlantLeadPlant, Make_Buy:"make", SortValue:"1"});
+					ClassTableData.push({id:index+1, Plant:Plantclass.title, Seq:"1",Status:"Current",MFG_Change: "MCONAME", MFG_Status: "Create",Change:"CA-00000777", Change_Status:"In Work", Oracle_Template: Plantclass.PlantOracleTemplate, ERP_Status:"true",ERP_Export:Plantclass.PlantERPExoprt, Lead_Plant:Plantclass.PlantLeadPlant, Make_Buy:"make", SortValue:"1"});
 				});
 				//ClassTableData = [
-				//	{id:1, Plant:"MVO", Seq:"1",Status:"Current",MFG_Change: "MCONAME", MFG_Status: "Create",Change:"CA-000004", ChangeStatus:"In Work", OracleTemplate:"template-003", ERPStatus:"true",ERP_Export:"Yes", Lead_Plant:"False", Make_Buy:"make", SortValue:"1"},
+				//	{id:1, Plant:"MVO", Seq:"1",Status:"Current",MFG_Change: "MCONAME", MFG_Status: "Create",Change:"CA-000004", Change_Status:"In Work", Oracle_Template:"template-003", ERPStatus:"true",ERP_Export:"Yes", Lead_Plant:"False", Make_Buy:"make", SortValue:"1"},
 				//	{id:1, Plant:"MMB", Seq:"1",Status:"Current",MFG_Change: "MCONAME", MFG_Status: "Create",Change:"CA-000004", ChangeStatus:"In Work", OracleTemplate:"template-004", ERPStatus:"true",ERP_Export:"Yes", Lead_Plant:"False", Make_Buy:"buy", SortValue:"2"},
 				//];
 				mainDiv.appendChild(whereUsedTable.showTable(ClassTableData,comWidget.getAssignedPlantTable(),"AssignedPlantsTable"));
